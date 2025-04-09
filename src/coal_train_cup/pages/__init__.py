147469d1ad8d182a3898ae6a20a832b2904f4246
Steven Burman <steven.burman@gmail.com>
2025-04-09 12:11:24 +1000

    Page refactor

--- src/coal_train_cup/pages/__init__.py
+++ src/coal_train_cup/pages/__init__.py
@@ -1,9 +1,7 @@
-from .main import (
-    page_make_tip,
-)
 from .page_view_round_tips import page_view_round_tips
 from .page_view_user_tips import page_view_user_tips
 from .page_leaderboard import page_leaderboard
+from .page_make_tip import page_make_tip
 
 __all__ = [
     "page_make_tip",
