94a726516cbc606858f627ec236015c3f88d2bb8
Steven Burman <steven.burman@gmail.com>
2025-04-25 10:19:25 +1000

    Add button clicked message

--- src/coal_train_cup/pages/page_make_tip.py
+++ src/coal_train_cup/pages/page_make_tip.py
@@ -115,6 +115,7 @@ def page_make_tip() -> None:
 
         if st.button("Submit tip"):
             try:
+                st.info("Congratulations, you clicked the button. Now wait. You will see a confirmation message when the tip is finalised...")
                 user_tip = make_tip(user, tip)
                 submit_tip(user_tip)
                 st.success("✅ Tip submitted. Choo choo, conductor.")
