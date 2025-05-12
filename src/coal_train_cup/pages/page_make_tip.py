88a082587b7fdbdc28b1b0d94d91faf3eb1dede7
Steven Burman <steven.burman@gmail.com>
2025-05-12 10:20:43 +1000

    Message change

--- src/coal_train_cup/pages/page_make_tip.py
+++ src/coal_train_cup/pages/page_make_tip.py
@@ -120,6 +120,6 @@ def page_make_tip() -> None:
                 )
                 user_tip = make_tip(user, tip)
                 submit_tip(user_tip)
-                st.success("✅ Tip submitted. Maybe this week, try getting one right for a change.")
+                st.success("✅ Tip submitted. Maybe this week you'll get one right for a change.")
             except Exception as e:
                 st.error(f"❌ Could not submit tip: {e}")
