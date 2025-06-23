950df92d5d8ab10afcecbeb45b1d09cd3c21bc57
Steven Burman <steven.burman@gmail.com>
2025-06-23 14:31:15 +1000

    Post r16 update

--- src/coal_train_cup/pages/page_make_tip.py
+++ src/coal_train_cup/pages/page_make_tip.py
@@ -120,6 +120,6 @@ def page_make_tip() -> None:
                 )
                 user_tip = make_tip(user, tip)
                 submit_tip(user_tip)
-                st.success("✅ Tip submitted. Game Two? More like Game Poo.")
+                st.success("✅ Tip submitted. I'm proud of your growth, Andy.")
             except Exception as e:
                 st.error(f"❌ Could not submit tip: {e}")
