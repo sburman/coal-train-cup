60da3e4d0ec327966204b381c670dc52d905711f
Steven Burman <steven.burman@gmail.com>
2025-09-09 16:46:22 +1000

    Real players

--- test_nrl_api.py
+++ test_nrl_api.py
@@ -4,5 +4,5 @@ from coal_train_cup.services.nrl_api_service import get_list_of_player_names_in_
 
 if __name__ == "__main__":
     # Test all endpoints to find player data
-    player_names = get_list_of_player_names_in_round(111, 2025, 27)
+    player_names = get_list_of_player_names_in_round(111, 2025, 28)
     print(player_names)
