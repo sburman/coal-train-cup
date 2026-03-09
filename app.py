adb11256ff3bb2f7a23d119c1a0cdc58e03cf65d
Steven Burman <steven.burman@gmail.com>
2026-03-09 12:13:30 +1000

    Bump

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def page_home() -> None:
     )
     st.markdown("---")
 
-    st.caption("_version: 2026.2.1_")
+    st.caption("_version: 2026.2.2_")
 
 
 HOME_PAGE = st.Page(page_home, title="Home", icon="🚂", default=True)
